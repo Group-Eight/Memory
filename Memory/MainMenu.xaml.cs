fbedf1e6a13d3992b8ec0ad8e1a99c4d5e8d931f
mHvNG <mail@mathijshoving.nl>
2020-09-23 12:28:26 +0200

    Visual Studio WPF project

---
+++ Memory/MainMenu.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Memory
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        StackPanel panel = new StackPanel();
+        public MainMenu()
+        {
+            InitializeComponent();
+            this.setText();
+            this.Content = panel;
+        }
+
+        private void setText() {
+            Label title = new Label();
+            title.Content = "Hello World";
+            panel.Children.Add(title);
+        }
+    }
+}
