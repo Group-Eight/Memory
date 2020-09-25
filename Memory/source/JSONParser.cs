58d3987e9ffe2c1510890d6d3d64c6efe2fe5d9c
mHvNG <mail@mathijshoving.nl>
2020-09-25 20:16:01 +0200

    [JSONParser] init

---
+++ Memory/source/JSONParser.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace Memory {
+    class JSONParser {
+
+        JObject content;
+
+        public JSONParser(string filePath) {
+            // Constructor
+            this.content = this.parse(filePath);
+        }
+
+        private JObject parse(string filePath) {
+            return;
+        }
+
+        public List<string> getUsers() {
+            return;
+        }
+    }
+}
