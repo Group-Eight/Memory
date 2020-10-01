efe3f87f3f39f11d27f2350d660a7b29e656054d
mHvNG <mail@mathijshoving.nl>
2020-10-01 10:27:03 +0200

    [JSONParser] getGrid

--- Memory/source/JSONParser.cs
+++ Memory/source/JSONParser.cs
@@ -2,20 +2,36 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
 using System.Security.RightsManagement;
+using System.Windows;
+using System.Windows.Media.Converters;
 
 namespace Memory {
     class JSONParser {
 
         JObject content;
 
+        /*
+         * < Usage >
+         * JSONParser parser = new JSONParser(filePath);
+         * parser.getTokens(key);
+         * parser.getGrid();
+         * 
+         * IMPORTANT - getTokens is not meant for the grid! getGrid is used to parse the grid.
+         * ALSO IMPORTANT - For every single file there needs to be an instance
+         */
         public JSONParser(string filePath) {
             // Constructor
             this.content = this.parse(filePath);
-            foreach (var i in this.getTokens("score")) { Console.WriteLine(i); }
-            //Console.WriteLine(this.getTokens("grid"));
         }
 
+        /*
+         * This method reads the given file.
+         * Arguments: filePath
+         * Return value: JObject
+         */
         private JObject parse(string filePath) {
             string content = "";
             using (StreamReader reader = new StreamReader(filePath)) {
@@ -24,6 +40,11 @@ namespace Memory {
             return JObject.Parse(content);
         }
 
+        /*
+         * This method reads and returns the value of the tokens.
+         * Arguments: key
+         * Return value: List
+         */
         public List<JToken> getTokens(string key) {
             List<JToken> tokens = new List<JToken> { };
             foreach (var x in this.content) { 
@@ -35,6 +56,30 @@ namespace Memory {
                 }
             }
             return tokens;
+        }
+
+        /*
+         * This method returns the grid with cell and card.
+         * Arguments: None
+         * Return value: Dictionary
+         */
+        public Dictionary<JToken, JToken> getGrid() {
+            Dictionary<JToken, JToken> grid = new Dictionary<JToken, JToken> { };
+            foreach (var x in this.content) {
+                if (x.Key == "grid") {
+                    foreach (var child in x.Value.Children()) {
+                        Regex valuePair = new Regex(@"[a-z]+\d", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                        MatchCollection collection = valuePair.Matches(child.ToString());
+                        List<string> keyValue = new List<string> { };
+                        foreach (Match match in collection) {
+                            GroupCollection col = match.Groups;
+                            keyValue.Add(col[0].ToString());
+                        }
+                        grid.Add(keyValue[0], keyValue[1]);
+                    }
+                }
+            }
+            return grid;
         }
     }
 }
