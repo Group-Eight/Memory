dd352fd59d61176367218d7b751a78a564e51425
HyperPandax <39954733+HyperPandax@users.noreply.github.com>
2020-10-09 13:52:25 +0200

    3 difficulties not optimal

--- Memory/MemoryCard.xaml.cs
+++ Memory/MemoryCard.xaml.cs
@@ -23,6 +23,7 @@ namespace Memory
         int savedImg = 0;
         List<Path> People;
         List<Canvas> Buildings;
+        List<int> BuildingWidths;
 
         private static Random rand = new Random();
 
@@ -32,11 +33,12 @@ namespace Memory
             Card.Background = new SolidColorBrush(Colors.Black);
             People = new List<Path> { Person1, Person2, Person3, Person4, Person5, Person6, Person7, Person8, Person9, Person10, Person11, Person12, Person13, Person14, Person15, Person16, Person17, Person18, Person19, Person20, Person21, Person22, Person23, Person24, Person25, Person26, Person27, Person28, Person29, Person30, Person31, Person32 };
             Buildings = new List<Canvas> { Building_1, Building_2, Building_3, Building_4 };
+            BuildingWidths = new List<int> { 50, 70, 90, 120 };
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            SetCards(3, 10);
+            SetCards(2, 10);
 
         }
         public int RandomValue(int min, int max)
@@ -52,11 +54,198 @@ namespace Memory
             if (difficulty == 1)
             {
                 Console.WriteLine("EZ: Gamemode 1");
+                for (int i = 0; i < amountcards; i++)
+                {
+
+                    Canvas[] MyRandomArray = Buildings.OrderBy(x => rand.Next()).ToArray();
+                    for (var e = 0; e < Buildings.Count; e++)
+                    {
+                        //set 1 house left and one right
+                        Canvas.SetLeft(MyRandomArray[0], 0);
+                        Canvas.SetRight(MyRandomArray[3], 0);
+
+                        if (MyRandomArray[0] == Building_1)
+                        {
+                            Canvas.SetLeft(MyRandomArray[1], rand.Next(BuildingWidths[0] - 10, BuildingWidths[0] + 10));
+                            if (MyRandomArray[1] == Building_2)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(100, 110));
+                            }
+                            else if (MyRandomArray[1] == Building_3)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(130, 140));
+                            }
+                            else if (MyRandomArray[1] == Building_4)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(150, 160));
+                            }
+
+                        }
+                        else if (MyRandomArray[0] == Building_2)
+                        {
+                            Canvas.SetLeft(MyRandomArray[1], rand.Next(BuildingWidths[1] - 10, BuildingWidths[1] + 10));
+                            if (MyRandomArray[1] == Building_1)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(80, 90));
+                            }
+                            else if (MyRandomArray[1] == Building_3)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(140, 160));
+                            }
+                            else if (MyRandomArray[1] == Building_4)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(180, 190));
+                            }
+                        }
+                        else if (MyRandomArray[0] == Building_3)
+                        {
+                            Canvas.SetLeft(MyRandomArray[1], rand.Next(BuildingWidths[2] - 10, BuildingWidths[2] + 10));
+                            if (MyRandomArray[1] == Building_1)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(130, 140));
+                            }
+                            else if (MyRandomArray[1] == Building_2)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(150, 160));
+                            }
+                            else if (MyRandomArray[1] == Building_4)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(190, 200));
+                            }
+                        }
+                        else if (MyRandomArray[0] == Building_4)
+                        {
+                            Canvas.SetLeft(MyRandomArray[1], rand.Next(BuildingWidths[3] - 10, BuildingWidths[3] + 10));
+                            if (MyRandomArray[1] == Building_1)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(150, 160));
+                            }
+                            else if (MyRandomArray[1] == Building_2)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(160, 170));
+                            }
+                            else if (MyRandomArray[1] == Building_3)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(180, 190));
+                            }
+                        }
+
+                    }
+
+                    /*Canvas.SetZIndex(moon, rand.Next(-1, 3));
+                    Canvas.SetLeft(moon, rand.Next(20, 180));
+                    Canvas.SetLeft(Lantern1, rand.Next(0, 100 + i * 10));
+                    Console.WriteLine(Canvas.GetLeft(Lantern1));
+                    Canvas.SetLeft(Lantern2, rand.Next(0, 250));*/
+
+                    SaveCanvasAsPng();
+
+                }
+                savedImg = 0;
+
             }
             // medium
             if (difficulty == 2)
             {
                 Console.WriteLine("Medium: Gamemode 2");
+                
+                for(int i = 0; i < amountcards; i++)
+                {
+                    
+                    Canvas[] MyRandomArray = Buildings.OrderBy(x => rand.Next()).ToArray();
+                    for (var e = 0; e < Buildings.Count; e++)
+                    {
+                        //set 1 house left and one right
+                        Canvas.SetLeft(MyRandomArray[0], 0);
+                        Canvas.SetRight(MyRandomArray[3], 0);
+
+                        if (MyRandomArray[0] == Building_1) {
+                            Canvas.SetLeft(MyRandomArray[1], rand.Next(BuildingWidths[0]- 10, BuildingWidths[0] +10)); 
+                            if (MyRandomArray[1] == Building_2) {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(100, 110));
+                            } else if (MyRandomArray[1] == Building_3) {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(130, 140));
+                            }
+                            else if (MyRandomArray[1] == Building_4) {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(150, 160));
+                            }   
+                        
+                        }else if (MyRandomArray[0] == Building_2) { 
+                            Canvas.SetLeft(MyRandomArray[1], rand.Next(BuildingWidths[1] - 10, BuildingWidths[1] + 10));
+                            if (MyRandomArray[1] == Building_1)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(80, 90));
+                            }
+                            else if (MyRandomArray[1] == Building_3)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(140, 160));
+                            }
+                            else if (MyRandomArray[1] == Building_4)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(180, 190));
+                            }
+                        }
+                        else if (MyRandomArray[0] == Building_3) { 
+                            Canvas.SetLeft(MyRandomArray[1], rand.Next(BuildingWidths[2] - 10, BuildingWidths[2] + 10));
+                            if (MyRandomArray[1] == Building_1)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(130, 140));
+                            }
+                            else if (MyRandomArray[1] == Building_2)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(150, 160));
+                            }
+                            else if (MyRandomArray[1] == Building_4)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(190, 200));
+                            }
+                        }
+                        else if (MyRandomArray[0] == Building_4) { 
+                            Canvas.SetLeft(MyRandomArray[1], rand.Next(BuildingWidths[3] - 10, BuildingWidths[3] + 10));
+                            if (MyRandomArray[1] == Building_1)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(150, 160));
+                            }
+                            else if (MyRandomArray[1] == Building_2)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(160, 170));
+                            }
+                            else if (MyRandomArray[1] == Building_3)
+                            {
+                                Canvas.SetLeft(MyRandomArray[2], rand.Next(180, 190));
+                            }
+                        }
+                       
+                       
+
+
+
+
+                    }
+
+                    Canvas.SetZIndex(moon, rand.Next(-1, 3));
+                    Canvas.SetLeft(moon, rand.Next(20, 180));
+                    Canvas.SetLeft(Lantern1, rand.Next(0, 100 + i * 10));
+                    Console.WriteLine(Canvas.GetLeft(Lantern1));
+                    Canvas.SetLeft(Lantern2, rand.Next(0, 250));
+
+                    //all people zindex -1
+                    ResetPeople();
+
+                    int randomP1 = rand.Next(0, 10);
+                    int randomP2 = rand.Next(10, 20);
+                    int randomP3 = rand.Next(20, 31);
+                    //Console.WriteLine("P1: " + randomP1 + " p2 " + randomP2 + " p3 " + randomP3);
+
+                    Canvas.SetZIndex(People[randomP1], 1);
+                    Canvas.SetZIndex(People[randomP2], 1);
+                    Canvas.SetZIndex(People[randomP3], 1);
+
+                    SaveCanvasAsPng();
+
+                }
+                savedImg = 0;
+
             }
             // hard
             if (difficulty == 3)
@@ -70,9 +259,10 @@ namespace Memory
 
                 for (int i = 0; i < amountcards; i++)
                 {
-                    Canvas.SetZIndex(moon, RandomValue(-1, 1));
-                    Canvas.SetLeft(moon, RandomValue(20, 180));
-                    Canvas.SetLeft(Lantern, RandomValue(10, 80));
+                    Canvas.SetZIndex(moon, rand.Next(-1, 3));
+                    Canvas.SetLeft(moon, rand.Next(20, 180));
+                    Canvas.SetLeft(Lantern1, rand.Next(0, 250)); 
+                    Canvas.SetLeft(Lantern2, rand.Next(0, 250));
 
                     //all people zindex -1
                     ResetPeople();
@@ -85,31 +275,8 @@ namespace Memory
                     Canvas.SetZIndex(People[randomP1], 1);
                     Canvas.SetZIndex(People[randomP2], 1);
                     Canvas.SetZIndex(People[randomP3], 1);
-                    
 
-
-                    Rect rect = new Rect(Card.RenderSize);
-                    RenderTargetBitmap rtb = new RenderTargetBitmap((int)rect.Right,
-                      (int)rect.Bottom, 96d, 96d, System.Windows.Media.PixelFormats.Default);
-                    rtb.Render(Card);
-                    //endcode as PNG
-                    BitmapEncoder pngEncoder = new PngBitmapEncoder();
-                    pngEncoder.Frames.Add(BitmapFrame.Create(rtb));
-
-                    //save to memory stream
-                    System.IO.MemoryStream ms = new System.IO.MemoryStream();
-
-                    pngEncoder.Save(ms);
-                    ms.Close();
-                    //img moet hier komen:  C:\Users\lisan\Documents\GitHub\Memory\Memory\
-                    //img komt nu hier:     C:\Users\lisan\Documents\GitHub\Memory\Memory\bin\debug
-
-
-
-                    System.IO.File.WriteAllBytes("Card" + savedImg + ".png", ms.ToArray());
-                    savedImg++;
-                    Console.WriteLine("Done");
-
+                    SaveCanvasAsPng();
                 }
                 savedImg = 0;
             }
@@ -125,7 +292,42 @@ namespace Memory
             
         }
         
+        private void SaveCanvasAsPng()
+        {
+            Card.UpdateLayout();
 
+            Rect bounds = VisualTreeHelper.GetDescendantBounds(Card);
+            double dpi = 96d;
+
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)bounds.Width, (int)bounds.Height, dpi, dpi, System.Windows.Media.PixelFormats.Default);
+
+            DrawingVisual dv = new DrawingVisual();
+            using (DrawingContext dc = dv.RenderOpen())
+            {
+                VisualBrush vb = new VisualBrush(Card);
+                dc.DrawRectangle(vb, null, new Rect(new Point(), bounds.Size));
+            }
+
+            rtb.Render(dv);
+
+            BitmapEncoder pngEncoder = new PngBitmapEncoder();
+            pngEncoder.Frames.Add(BitmapFrame.Create(rtb));
+
+            try
+            {
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+
+                pngEncoder.Save(ms);
+                ms.Close();
+
+                System.IO.File.WriteAllBytes("Card" + savedImg + ".png", ms.ToArray());
+                savedImg++;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
 
     }
 }
